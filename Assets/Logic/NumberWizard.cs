93ffb23c3518d09741516577ed937621d74b48c7
dzmitrytrystsen <tristendmitry@gmail.com>
2018-10-03 17:26:37 +0300

    WebGL ready
    +

--- Assets/Logic/NumberWizard.cs
+++ Assets/Logic/NumberWizard.cs
@@ -1,53 +1,46 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
-public class NumberWizard : MonoBehaviour {
+public class NumberWizard : MonoBehaviour
+{
 
-    int max;
-    int min;
+    [SerializeField] int max;
+    [SerializeField] int min;
+    [SerializeField] Text guessText;
+
     int guess;
 
-    void Start()
+    public void Start()
     {
         StartGame();
     }
 
-    void StartGame()
+    public void StartGame()
     {
-        max = 1000;
-        min = 1;
-        guess = 500;
-
-        Debug.Log("The highest number you can pick is: " + max);
-        Debug.Log("The lowest number you can pick is: " + min);
-        Debug.Log("Tell me if your number is higher or lower than: " + guess);
-        Debug.Log("Push Up - Higher, Push Down - Lower, Push Enter - Correct");
-        max = max++;
+        max = ++max;
+        guess = (max + min) / 2;
+        guessText.text = guess.ToString();
     }
 
-    void Update()
+    public void OnPressHigher()
     {
-        if (Input.GetKeyDown(KeyCode.UpArrow))
-        {
-            min = guess;
-            NextGuess();
-        }
-        else if (Input.GetKeyDown(KeyCode.DownArrow))
-        {
-            max = guess;
-            NextGuess();
-        }
-        else if (Input.GetKeyDown(KeyCode.Return))
-        {
-            Debug.Log("I am a genius!");
-            StartGame();
-        }
+        min = guess;
+        NextGuess();
+        guessText.text = guess.ToString();
     }
-    void NextGuess()
+
+    public void OnPressLower()
+    {
+        max = guess;
+        NextGuess();
+        guessText.text = guess.ToString();
+    }
+
+    public void NextGuess()
     {
         guess = (max + min) / 2;
-        Debug.Log("Is it higher or lower than: " + guess);
     }
 }
 
