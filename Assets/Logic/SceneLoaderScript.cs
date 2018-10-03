4c7195c16401e5aa63110a2d778ddff22404f03a
dzmitrytrystsen <tristendmitry@gmail.com>
2018-10-03 12:28:30 +0300

    Buttons ready

---
+++ Assets/Logic/SceneLoaderScript.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoaderScript : MonoBehaviour
+{
+    public void LoadNextScene()
+    {
+        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+
+        SceneManager.LoadScene(++currentSceneIndex);
+    }
+
+}
